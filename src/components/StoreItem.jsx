e943427d081f6e28d1836941c9260648ea745f5b
MEZ901 <issammez44@gmail.com>
2023-10-23 13:38:21 +0100

    set quantity functions locally

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -6,9 +6,22 @@ import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useState } from "react";
 
 const StoreItem = ({ element }) => {
-  const quantity = 5;
+  const [quantity, setQuantity] = useState(0);
+
+  const increaseCartQuantity = (id) => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const decreaseCartQuantity = (id) => {
+    setQuantity((prev) => prev - 1);
+  };
+
+  const removeFromCart = (id) => {
+    setQuantity(0);
+  };
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -36,17 +49,21 @@ const StoreItem = ({ element }) => {
       <CardActions disableSpacing>
         <div className="w-full flex justify-center">
           {quantity === 0 ? (
-            <Button variant="contained" sx={{ width: "100%" }}>
+            <Button
+              variant="contained"
+              sx={{ width: "100%" }}
+              onClick={() => increaseCartQuantity(element.id)}
+            >
               + Add To Cart
             </Button>
           ) : (
             <div className="w-full flex items-center flex-col">
               <div className="flex items-center justify-center gap-3">
-                <IconButton>
+                <IconButton onClick={() => increaseCartQuantity(element.id)}>
                   <AddIcon />
                 </IconButton>
                 <Typography>{quantity} in cart</Typography>
-                <IconButton>
+                <IconButton onClick={() => decreaseCartQuantity(element.id)}>
                   <RemoveIcon />
                 </IconButton>
               </div>
@@ -54,6 +71,7 @@ const StoreItem = ({ element }) => {
                 variant="contained"
                 color="warning"
                 sx={{ width: "100%" }}
+                onClick={() => removeFromCart(element.id)}
               >
                 Remove
               </Button>
